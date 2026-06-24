354e0749b8946b87af9e85f1297435aa41c15109
Jordan Russell <jordan.russell@sitehost.co.nz>
2026-06-24 12:35:36 +1200

    fix: make PyZ3950 compatible with Python 3.14
    The library failed to import on Python 3.11+ and broke at runtime on
    3.12+. Restore compatibility and modernise the build and CI for current
    Python, dropping the dead Python 2 support.
    
    - ccl.py: use scoped inline regex flags (?i:...) in the PLY token rules;
      Python 3.11+ rejects global flags that aren't at the start of the
      combined pattern
    - ccl.py: replace removed string.atoi() with int()
    - asn1.py: replace removed string.join(seq, sep) with sep.join(seq)
    - setup.py: migrate from distutils (removed in 3.12) to setuptools, fix
      classifiers to a list, add python_requires and Py3 classifiers, and
      drop the vestigial no-op build_ext/Extension machinery
    - tox.ini: target py39-py314, drop py27/py35
    - CI: remove the Python 2.7 job, bump checkout/setup-python actions, pin
      Python 3.14, pyupgrade --py39-plus
    - requirements_test.txt: drop pytest-pythonpath, which capped pytest to a
      version that fails on 3.14 (ast.Str removed in 3.12)
    - gitignore *.egg-info/ generated by editable installs

--- setup.py
+++ setup.py
@@ -1,48 +1,22 @@
 #!/usr/bin/env python
-from __future__ import print_function
-
-from distutils.core import setup
+from setuptools import setup
 
 import vers
-import os
-import os.path
 
-# Because PLY compiles the yacc grammar to Python code, we need to run
-# that compilation at install time to avoid dropping the created files
-# in some random directory.  (Note that PLY's staleness checking
-# depends on the MD5 hash of the repr() of assorted objects, and the
-# repr is subject to change between Python versions (maybe)).
-
-# I can't find a consistent way to import modules at build time,
-# so I've removed this code for now.  (Changing directories into
-# PyZ3950 works under Python 2.2 but not 2.3, importing with pathnames
-# works under Linux but not Win 98 or XP.)
-
-
-# We define a custom build_ext
-
-from distutils.util import byte_compile
-from distutils.command.build_ext import build_ext
-from distutils.extension import Extension
-
-pyz_dir = "PyZ3950"
-
-class PLYBuild(build_ext):
-    def run(self):
-        for ext in self.extensions:
-            nm =  self.get_ext_fullname (ext.sources[0])
-#            print("running %s to generate parsing tables" % (nm,))
-#
-#            mod = __import__ (os.path.join (pyz_dir,nm))
-
-
-
-foo = Extension ("parsetab", ["ccl"])
+# PLY compiles the yacc/lex tables to Python at import time (see ccl.py),
+# so no build-time extension step is needed here.
 
 classifiers = """\
 Development Status :: 5 - Production/Stable
 Intended Audience :: Developers
 Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
+Programming Language :: Python :: 3.12
+Programming Language :: Python :: 3.13
+Programming Language :: Python :: 3.14
 Topic :: Internet :: Z39.50"""
 
 setup (name="PyZ3950",
@@ -55,9 +29,7 @@ setup (name="PyZ3950",
        """Pure Python implementation of ASN.1 and Z39.50 v3,
        with a simple MARC parser thrown in.  See the URL for details.""",
        platforms = ["any"],
-       classifiers = filter(None, classifiers.split("\n")),
+       classifiers = list(filter(None, classifiers.split("\n"))),
        url = "http://www.pobox.com/~asl2/software/PyZ3950",
        packages = ["PyZ3950"],
-       cmdclass = {'build_ext' : PLYBuild},
-       ext_modules= [foo])
-
+       python_requires = ">=3.9")
